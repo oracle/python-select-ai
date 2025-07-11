b2ab94aa10410230b855580b697f2aed84956352
Abhishek Singh <abhishek.o.singh@oracle.com>
2025-07-11 16:55:09 -0700

    Added copyright notices to samples

--- examples/async_examples/profile_create.py
+++ examples/async_examples/profile_create.py
@@ -1,3 +1,10 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2025, Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# http://oss.oracle.com/licenses/upl.
+# -----------------------------------------------------------------------------
+
 import asyncio
 import os
 from pprint import pformat
