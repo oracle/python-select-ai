ba383b800751ee161cdd17a945721847e24256d2
Abhishek Singh <abhishek.o.singh@oracle.com>
2026-07-30 11:14:34 -0700

    Skip tests which is blocking merges due to intermittent failures

--- tests/agents/test_3800_async_agente2e.py
+++ tests/agents/test_3800_async_agente2e.py
@@ -30,6 +30,10 @@ from select_ai.agent import (
     ToolParams,
 )
 
+pytestmark = pytest.mark.skip(
+    reason="Temporarily skipped due to intermittent failures. This needs to be root caused before enabling"
+)
+
 pytestmark = pytest.mark.anyio
 
 # ----------------------------------------------------------------------
